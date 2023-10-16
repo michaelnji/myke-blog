38217717d18a927b32fd347609fa3049b8cf9cb9
michaelnji <73862378+michaelnji@users.noreply.github.com>
2023-10-16 18:48:18 +0200

    chore: :heavy_plus_sign: sdding necessary dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,34 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import vercel from "@astrojs/vercel/serverless";
+import partytown from "@astrojs/partytown";
+import prefetch from "@astrojs/prefetch";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import rehypeExternalLinks from "rehype-external-links";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://myke-blog.vercel.app",
+  integrations: [
+    svelte(),
+    partytown(),
+    prefetch(),
+    mdx(),
+    sitemap(),
+    tailwind(),
+  ],
+  output: "server",
+  adapter: vercel(),
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          content: { type: "text", value: " 🔗" },
+        },
+      ],
+    ],
+  },
+});
